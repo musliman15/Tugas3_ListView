ef00297d43cfb738fdc931e306527cb6abc61f6c
Muhsan008 <thechuns08@gmail.com>
2022-01-06 17:55:32 +0800

    Initial commit

---
+++ app/src/main/java/simple/example/hewanpedia/DataProvider.java
@@ -0,0 +1,84 @@
+package simple.example.hewanpedia;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import simple.example.hewanpedia.model.Anjing;
+import simple.example.hewanpedia.model.Hewan;
+import simple.example.hewanpedia.model.Kucing;
+import simple.example.hewanpedia.model.Ular;
+
+public class DataProvider {
+    private static List<Hewan> hewans = new ArrayList<>();
+
+    private static List<Kucing> initDataKucing(Context ctx) {
+        List<Kucing> kucings = new ArrayList<>();
+        kucings.add(new Kucing("Angora", "Turki",
+                "Aslinya berasal dari Ankara, Turki dengan ciri khas berbulu panjang dan lembut", R.drawable.cat_angora));
+        kucings.add(new Kucing("Bengal", "Inggris",
+                "Merupakan hasil persilangan antara Kucing Leopard Asia dengan kucing domestik Inggris", R.drawable.cat_bengal));
+        kucings.add(new Kucing("Birmani", "Birma/Myanmar",
+                "Konon awalnya merupakan ras kucing yand biasa menemani para biksu yang menetap pada kuil di Birma", R.drawable.cat_birman));
+        kucings.add(new Kucing("Persia", "Iran",
+                "Kucing berbulu panjang dengan wajah bundar dan hidung pesek. Awalnya berasal dari Iran kemudian diimpor ke Italia", R.drawable.cat_persia));
+
+        return kucings;
+    }
+
+    private static List<Anjing> initDataAnjing(Context ctx) {
+        List<Anjing> anjings = new ArrayList<>();
+        anjings.add(new Anjing("Bulldog", "Inggris",
+                "Anjing populer yang dikenal dengan badan yang gemuk berotot, wajah seperti kain kusut, dan hidung yang pesek", R.drawable.dog_bulldog));
+        anjings.add(new Anjing("Husky", "Alaska,Siberia,Finlandia (daerah bersalju) ",
+                "Anjing jenis khusus yang awalnya digunakan untuk menarik kereta salju", R.drawable.dog_husky));
+        anjings.add(new Anjing("Kintamani", "Indonesia",
+                "Ras anjing asli pulau Bali ", R.drawable.dog_kintamani));
+        anjings.add(new Anjing("Samoyed", "Rusia",
+                "Anjing yang berasal dari Siberia yang awalnya biasa digunakan untuk menjaga ternak oleh suku Samoyed", R.drawable.dog_samoyed));
+
+        return anjings;
+    }
+
+    private static List<Ular> intDataUlar(Context ctx) {
+        List<Ular> ulars = new ArrayList<>();
+        ulars.add(new Ular("Erpeton", "Asia Tenggara",
+                "Ular kecil ini hidup di air keruh.mereka kebanyakan makan ikan.", R.drawable.ular_erpeton));
+        ulars.add(new Ular("Viper", "Afrika Tengah",
+                "Ular ini biasa memakan hewan darat seperti mamalia dan katak.ia juga sering disebut ular berbungan.", R.drawable.ular_hairybush));
+        ulars.add(new Ular("Malagasi", "Madagaskar",
+                "Ular ini memiliki moncong panjang yang menyerupai daun datar.", R.drawable.ular_langaha));
+        ulars.add(new Ular("Viper" ,"Tanzania",
+                "Ular ini memiliki tanduk yang terlihat keren tepat di atas mata besarnya.habitat alami mereka terancam punah akibat penebangan pohon liar.", R.drawable.ular_matilda));
+        
+        return ulars;
+    }
+
+    private static void initAllHewans(Context ctx) {
+        hewans.addAll(initDataKucing(ctx));
+        hewans.addAll(initDataAnjing(ctx));
+        hewans.addAll(intDataUlar(ctx));
+    }
+
+    public static List<Hewan> getAllHewan(Context ctx) {
+        if (hewans.size() == 0) {
+            initAllHewans(ctx);
+        }
+        return  hewans;
+    }
+
+    public static List<Hewan> getHewansByTipe(Context ctx, String jenis) {
+        List<Hewan> hewansByType = new ArrayList<>();
+        if (hewans.size() == 0) {
+            initAllHewans(ctx);
+        }
+        for (Hewan h : hewans) {
+            if (h.getJenis().equals(jenis)) {
+                hewansByType.add(h);
+            }
+        }
+        return hewansByType;
+    }
+
+}
